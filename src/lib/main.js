02fff36ad6b67e2d2e77054a5edbef26a541912d
Artem <gusev2014russia@mail.ru>
2019-08-03 21:13:23 +0300

    add description

--- src/lib/main.js
+++ src/lib/main.js
@@ -6,41 +6,6 @@ const evaluate = require('./evaluate');
 
 // создание глобального контекста
 const globalEnv = new Environment();
-// const code = "sum = lambda(x, y) x + y; print(sum(2, 3));";
-// const code = `print_range = CRB(a, b) {
-//                 if a <= b {
-//                   print(a);
-//                   if a + 1 <= b {
-//                     print(", ");
-//                     print_range(a + 1, b);
-//                   } else println("");
-//                 }
-//               };
-//               print_range(1, 10);
-// `;
-
-// const code=`cons = CRB(a, b) CRB(f) f(a, b);
-// car = CRB(cell) cell(CRB(a, b) a);
-// cdr = CRB(cell) cell(CRB(a, b) b);
-// NIL = CRB(f) f(NIL, NIL);
-//
-// x = cons(1, cons(2, cons(3, cons(4, cons(5, NIL)))));
-// # It's comment
-// println(car(x));                      # 1
-// println(car(cdr(x)));                 # 2
-// println(car(cdr(cdr(x))));            # 3
-// println(car(cdr(cdr(cdr(x)))));       # 4
-// println(car(cdr(cdr(cdr(cdr(x))))));  # 5
-//
-// # keywords 'CRB' and 'CreativeRusBear' are defined function
-// y = CreativeRusBear (a, b) {
-//     println(a+b);
-//     println("Hello world");
-// };
-// y(1, 7);
-// `;
-
-
 const code =`cons = CRB(x, y)
          CRB(a, i, v)
            if a == "get"
